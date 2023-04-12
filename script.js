cfc5b71f7a5977586ab71464fa9125d1876d7cf5
Luan_ <88367567+Luan16p@users.noreply.github.com>
2023-04-12 13:19:27 -0400

    Adicionando arquivos e preparando pro deploy

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const button = document.getElementById('add-img');
+const divImages = document.querySelector(".images");
+
+async function fetchDogApi() {
+    const fetchapi = await fetch("https://dog.ceo/api/breeds/image/random")
+        .then((res) => res.json())
+        .then((data) => {
+            divImages.innerHTML += `<img width=200 height=200 src=${data.message} alt="random-dog"/>`
+        });
+}
+
+button.addEventListener("click", ()=> {
+    fetchDogApi();
+})
